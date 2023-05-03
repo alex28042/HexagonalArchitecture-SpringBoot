2f376ddae788b175f0b2b0eade68e61ba3715127
alex28042 <alex28042@gmail.com>
2023-05-03 15:29:05 +0200

    kafka producer

---
+++ src/main/java/com/example/App/Users/Infrastructure/KafkaProducer.java
@@ -0,0 +1,15 @@
+package com.example.App.Users.Infrastructure;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void send(String topic, String message) {
+        kafkaTemplate.send(topic, message);
+    }
+}
